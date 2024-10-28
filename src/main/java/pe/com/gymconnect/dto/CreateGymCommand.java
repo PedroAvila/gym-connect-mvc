9868fd0b512fa365906f098db7193ea48584c79d
Pedro Avila <pedroulisesavila@gmail.com>
2024-10-28 17:47:59 -0500

    avance de llamar fragmentos para gym

--- src/main/java/pe/com/gymconnect/dto/CreateGymCommand.java
+++ src/main/java/pe/com/gymconnect/dto/CreateGymCommand.java
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.Size;
 
 public record CreateGymCommand(
 
-        @NotBlank String name,
+                @NotBlank String name,
 
-        @NotBlank String address,
+                @NotBlank String address,
 
-        @NotBlank @Size(max = 10, message = "El número de teléfono no puede tener más de 10 caracteres") String phone
+                @NotBlank @Size(max = 10, message = "El número de teléfono no puede tener más de 10 caracteres") String phone
 
 ) {
 }
